604347a58efe706c95c79527ae3773403fdb5a55
dynamic-henry-kwon <jukdo67@gmail.com>
2021-10-21 16:53:09 +0900

    Home 화면수정

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,11 @@
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
-    <router-view></router-view>
+    <v-app>
+      <v-content>
+        <router-view></router-view>
+      </v-content>
+    </v-app>
   </div>
 </template>
 <style src="./css/style.css"></style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,25 @@
 <template>
-  <div class="home">
-    <div class="inner">
-      <div class="title">하나금융TI 은행채널팀 뷰연구회 과제</div>
-      <div></div>
-    </div>
-  </div>
+  <v-container fluid grid-list-md>
+    <v-layout row wrap>
+      <v-flex v-for="n in 6" :key="n" xl4 lg4 md4 sm6 xs12>
+        <v-card width="100%" height="400px" color="#26c6da">
+          <v-btn
+            color="#26c6da"
+            class="body-1"
+            width="100%"
+            height="100%"
+            fill-height
+            v-bind:style="{
+              'background-image':
+                'url(' + require('../assets/ojingeGame.png') + ')',
+              backgroundSize: 'cover',
+            }"
+            >Inquire Item</v-btn
+          >
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
