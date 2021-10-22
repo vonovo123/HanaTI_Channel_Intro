c565eab47c8528697fd34081e9a06ceedbadeee7
dynamic-henry-kwon <jukdo67@gmail.com>
2021-10-22 17:00:50 +0900

    인덱스 화면 수정

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,23 @@
 <template>
   <div>
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
     <v-app>
-      <v-content>
-        <router-view></router-view>
-      </v-content>
+      <div class="header">
+        <div class="title">
+          <router-link to="/" class="headRouter">게임보드</router-link>
+        </div>
+        <div class="about">
+          <!--  |
+          <router-link to="/about">About</router-link> -->
+          <router-link to="about" class="headRouter">어바웃어스</router-link>
+        </div>
+      </div>
+      <div class="body">
+        <v-content>
+          <v-container fluid grid-list-md>
+            <router-view></router-view>
+          </v-container>
+        </v-content>
+      </div>
     </v-app>
   </div>
 </template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,25 +1,22 @@
 <template>
-  <v-container fluid grid-list-md>
-    <v-layout row wrap>
-      <v-flex v-for="n in 6" :key="n" xl4 lg4 md4 sm6 xs12>
-        <v-card width="100%" height="400px" color="#26c6da">
-          <v-btn
-            color="#26c6da"
-            class="body-1"
-            width="100%"
-            height="100%"
-            fill-height
-            v-bind:style="{
-              'background-image':
-                'url(' + require('../assets/ojingeGame.png') + ')',
-              backgroundSize: 'cover',
-            }"
-            >Inquire Item</v-btn
-          >
-        </v-card>
-      </v-flex>
-    </v-layout>
-  </v-container>
+  <v-layout row wrap class="pa-4">
+    <v-flex v-for="n in 6" :key="n" xl4 lg4 md4 sm6 xs12 class="gameBox pa-2">
+      <div class="tag">tag</div>
+      <v-card width="100%" height="300px">
+        <v-btn
+          class="homeBtn"
+          width="100%"
+          height="100%"
+          fill-height
+          v-bind:style="{
+            //'background-image':
+            //'url(https://i.pinimg.com/originals/ea/37/df/ea37df8d51c2c4bcc27c1708fff6c9d6.jpg)',
+            //'url(' + require('../assets/ojingeGame.png') + ')',
+          }"
+        ></v-btn>
+      </v-card>
+    </v-flex>
+  </v-layout>
 </template>
 
 <script>
